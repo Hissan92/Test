d89880ce929a2bbfc782d026e5b67323e78e081c
hissa <hissa@DESKTOP-IG662BI>
2019-02-15 16:17:06 -0600

    Added addition method

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 10, b = 5;
+		int c = a - b;
+		System.out.println(c);
+		
 
 	}
 
